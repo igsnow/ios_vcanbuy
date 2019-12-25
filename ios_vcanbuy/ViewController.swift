966e1db6e835c82a3ac05dcd01038d7b725bdc3d
igsnow <zzy716810918@gmail.com>
2019-12-25 14:35:48 +0800

    初始化swift项目

---
+++ ios_vcanbuy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios_vcanbuy
+//
+//  Created by 张志勇 on 2019/12/25.
+//  Copyright © 2019 张志勇. All rights reserved.
+//
+
+import UIKit
+import WebKit
+class ViewController: UIViewController, WKUIDelegate {
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let myURL = URL(string:"https://www.douyu.com")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
+    }}
