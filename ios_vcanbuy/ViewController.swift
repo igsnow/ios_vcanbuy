af897813f4fa1e937db4acf508a6fda28ddc4f38
igsnow <zzy716810918@gmail.com>
2019-12-27 15:06:06 +0800

    切换项目域名

--- ios_vcanbuy/ViewController.swift
+++ ios_vcanbuy/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let myURL = URL(string:"https://www.youku.com")
+        let myURL = URL(string:"http://m.vcanbuy.com")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }}
