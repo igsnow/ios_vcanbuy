19509514652e280d026010814498510a158ce8a8
igsnow <zzy716810918@gmail.com>
2020-01-09 21:58:18 +0800

    添加pod依赖，暴露给js置空剪切板的方法，发送消息给js的方法还待研究

--- ios_vcanbuy/ViewController.swift
+++ ios_vcanbuy/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 import WebKit
-class ViewController: UIViewController, WKUIDelegate {
+import SwiftyJSON
+
+class ViewController: UIViewController, WKUIDelegate , WKScriptMessageHandler{
     
     var webView: WKWebView!
     
@@ -22,7 +24,40 @@ class ViewController: UIViewController, WKUIDelegate {
         super.viewDidLoad()
         
 //        let myURL = URL(string:"http://m.vcanbuy.com")
-        let myURL = URL(string:"http://120.27.228.29:8081")
+//        let myURL = URL(string:"http://120.27.228.29:8081")
+        let myURL = URL(string:"http://169.254.23.141:1017")
+
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
-    }}
+        
+    }
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        self.dealWith(message: message)
+    }
+    
+    
+    func dealWith(message: WKScriptMessage) {
+        guard let messageBody = message.body as? String else {
+            return
+        }
+        let body = JSON.init(parseJSON: messageBody)
+        print(body)
+        
+        let action = body["action"].stringValue
+        _ = body["callbackId"].stringValue
+        if action.isEmpty {
+            return
+        }
+        // js调用swift方法置空剪切板
+        switch action {
+        case "deleteClipboardRecord":
+            UIPasteboard.general.string = "";
+        default:
+            return
+        }
+    }
+    
+}
+
+
