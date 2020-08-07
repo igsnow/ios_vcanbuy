212707b4fc566df3b7552637121b61340a3da3fa
igsnow <zzy716810918@gmail.com>
2020-08-07 10:26:01 +0800

    编写获取验证码倒计时按钮

--- ios_vcanbuy/PwdViewController.swift
+++ ios_vcanbuy/PwdViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class PwdViewController: UIViewController {
     
+    var sendButton: UIButton!
+   
+    var countdownTimer: Timer?
+   
+    var remainingSeconds: Int = 0 {
+        willSet {
+            sendButton.setTitle("(\(newValue)秒后重新获取)", for: .normal)
+           
+            if newValue <= 0 {
+                sendButton.setTitle("获取验证码", for: .normal)
+                isCounting = false
+            }
+        }
+    }
+    
+    var isCounting = false {
+        willSet {
+            if newValue {
+                countdownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(PwdViewController.updateTime(_:)), userInfo: nil, repeats: true)
+                
+                remainingSeconds = 5
+                
+                sendButton.backgroundColor = UIColor.gray
+            } else {
+                countdownTimer?.invalidate()
+                countdownTimer = nil
+                
+                sendButton.backgroundColor = UIColor.orange
+            }
+            
+            sendButton.isEnabled = !newValue
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,6 +70,15 @@ class PwdViewController: UIViewController {
         titleLabel.textAlignment = .center
         self.view.addSubview(titleLabel)
         
+        sendButton = UIButton()
+        sendButton.frame = CGRect(x: frame.width - 130, y: 100, width: 120, height: 40)
+        sendButton.backgroundColor = UIColor.orange
+        sendButton.setTitleColor(UIColor.white, for: .normal)
+        sendButton.titleLabel?.font = UIFont.systemFont(ofSize: 15)
+        sendButton.setTitle("获取验证码", for: .normal)
+        sendButton.addTarget(self, action: #selector(PwdViewController.sendButtonClick(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(sendButton)
         
         
        
@@ -48,6 +91,19 @@ class PwdViewController: UIViewController {
         let rootVC = UIApplication.shared.delegate as! AppDelegate
         let RootVC = RootViewController()
         rootVC.window?.rootViewController = RootVC
+    }
+    
+    @objc func sendButtonClick(_ sender: UIButton) {
+        isCounting = true
+    }
+    
+    @objc func updateTime(_ timer: Timer) {
+        remainingSeconds -= 1
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 }
