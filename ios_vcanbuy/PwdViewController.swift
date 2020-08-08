4169397343ffc11517c60e3215411fece323cb61
igsnow <zzy716810918@gmail.com>
2020-08-08 13:03:09 +0800

    编写验证码和密码输入框,调试样式

--- ios_vcanbuy/PwdViewController.swift
+++ ios_vcanbuy/PwdViewController.swift
@@ -55,7 +55,7 @@ class PwdViewController: UIViewController {
         self.view.backgroundColor = UIColor.white
         
         // 返回首页按钮
-        let backBtn:UIButton = UIButton(frame: CGRect(x: 0, y: 50, width: 50, height: 30))
+        let backBtn:UIButton = UIButton(frame: CGRect(x: 15, y: 50, width: 50, height: 30))
         backBtn.backgroundColor = UIColor.orange
         backBtn.setTitle("Back", for: .normal)
         backBtn.addTarget(self, action: #selector(backBtnAction), for: .touchUpInside)
@@ -76,7 +76,7 @@ class PwdViewController: UIViewController {
         self.view.addSubview(titleLabel)
         
         sendButton = UIButton()
-        sendButton.frame = CGRect(x: frame.width - 140, y: 100, width: 130, height: 40)
+        sendButton.frame = CGRect(x: frame.width - 145, y: 120, width: 130, height: 50)
         sendButton.backgroundColor = UIColor.orange
         sendButton.setTitleColor(UIColor.white, for: .normal)
         sendButton.titleLabel?.font = UIFont.systemFont(ofSize: 14)
@@ -85,6 +85,41 @@ class PwdViewController: UIViewController {
         
         self.view.addSubview(sendButton)
         
+        let otpField = UITextField(frame: CGRect(x:15, y:120, width:frame.width - 165, height:50))
+        otpField.borderStyle = UITextField.BorderStyle.line
+        otpField.placeholder="请输入验证码"
+        otpField.adjustsFontSizeToFitWidth=true  //当文字超出文本框宽度时，自动调整文字大小
+        otpField.minimumFontSize=14  //最小可缩小的字号
+        otpField.textAlignment = .left //水平居中对齐
+        otpField.contentVerticalAlignment = .center  //垂直居中对齐
+        otpField.clearButtonMode = .whileEditing  //编辑时出现清除按钮
+        let paddingOtpView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 20))
+        otpField.leftView = paddingOtpView
+        otpField.leftViewMode = .always
+        self.view.addSubview(otpField)
+        
+        let pwdField = UITextField(frame: CGRect(x:15, y:180, width:frame.width - 30, height:50))
+        pwdField.borderStyle = UITextField.BorderStyle.line
+        pwdField.placeholder="请输入新密码"
+        pwdField.adjustsFontSizeToFitWidth=true
+        pwdField.minimumFontSize=14
+        pwdField.textAlignment = .left
+        pwdField.contentVerticalAlignment = .center
+        pwdField.clearButtonMode = .whileEditing
+        let paddingPwdView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: 20))
+        pwdField.leftView = paddingPwdView
+        pwdField.leftViewMode = .always
+        self.view.addSubview(pwdField)
+        
+        let confirmButton = UIButton()
+        confirmButton.frame = CGRect(x: 15, y: 250, width: frame.width - 30, height: 50)
+        confirmButton.backgroundColor = UIColor.orange
+        confirmButton.setTitleColor(UIColor.white, for: .normal)
+        confirmButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        confirmButton.setTitle("确认修改", for: .normal)
+        confirmButton.addTarget(self, action: #selector(PwdViewController.sendButtonClick(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(confirmButton)
     
         
     }
